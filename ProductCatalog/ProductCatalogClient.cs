d0833ab9f6e25056729076d78fce277b3ed134b6
Andrew Allshouse <allshouseandrew@aol.com>
2017-05-30 16:34:24 -0400

    Call apiary product catalog and deserialize response

--- ProductCatalog/ProductCatalogClient.cs
+++ ProductCatalog/ProductCatalogClient.cs
@@ -1,10 +1,15 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Polly;
 
 public interface IProductCatalogClient
 {
-  Task<IEnumerable<Item>> GetShoppingCartItems(IEnumerable<int> ids);
+  Task<IEnumerable<Item>> GetShoppingCartItems(int[] productCatalogIds);
 }
 
 public class ProductCatalogClient : IProductCatalogClient
@@ -13,15 +18,43 @@ public class ProductCatalogClient : IProductCatalogClient
     @"http://private-7d5bc3-productcatalog18.apiary-mock.com";
   private static string getProductPathTemplate = 
     "/products?productIds=[{0}]";
-
-  private IEnumerable<Item> productCatalog;
-
-  public async Task<IEnumerable<Item>> GetShoppingCartItems(IEnumerable<int> ids)
+  
+  private static async Task<HttpResponseMessage> RequestProductFromCatalog(int[] productCatalogIds)
   {
-    //TODO: add an await here
-    var items = productCatalog.Where(item => ids.Contains(item.ProductCatalogId));
+    var productsResource = string.Format(getProductPathTemplate, string.Join(",", productCatalogIds));
+    //Console.WriteLine($"productsResource: {productsResource}");
+    using(var httpClient = new HttpClient())
+    {
+      httpClient.BaseAddress = new Uri(productCatalogBaseUrl);
+      return await httpClient.GetAsync(productsResource).ConfigureAwait(false);
+    }
+  }
 
-    return items ?? Enumerable.Empty<Item>();
+  private static async Task<IEnumerable<Item>> ConvertToShoppingCartItems(HttpResponseMessage response)
+  {
+    response.EnsureSuccessStatusCode();
+    var products = JsonConvert.DeserializeObject<List<Item>>(
+      await response.Content.ReadAsStringAsync().ConfigureAwait(false));
+    //Note: Most times the deserialized object will not match the local object exactly
+    //They will have to be translated with LINQ
+    return products;
+  }
+
+  private async Task<IEnumerable<Item>> GetItemsFromCatalogService(int[] productCatalogIds)
+  {
+    var response = await RequestProductFromCatalog(productCatalogIds).ConfigureAwait(false);
+    return await ConvertToShoppingCartItems(response).ConfigureAwait(false);
+  }
+
+  private static Policy exponentialRetryPolicy = 
+    Policy.Handle<Exception>()
+    .WaitAndRetryAsync(3, attempt => 
+      TimeSpan.FromMilliseconds(100 * Math.Pow(2, attempt)));
+
+  public async Task<IEnumerable<Item>> GetShoppingCartItems(int[] productCatalogIds)
+  {
+    return await exponentialRetryPolicy.ExecuteAsync(async () => 
+      await GetItemsFromCatalogService(productCatalogIds).ConfigureAwait(false));
   }
 
   //Add a failure handling policy around the call to the Product Catalog Microservice
